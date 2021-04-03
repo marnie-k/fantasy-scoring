f15e1b19e8fedb4b8b99de3b2ae3888e04ebbc45
Marnie K <marnie.kelly.123@gmail.com>
2021-04-03 10:05:16 -0400

    adding index.js. Written in pseudo code right now. Need to figure out correct syntax

---
+++ index.js
@@ -0,0 +1,16 @@
+//I need to reference all the external files first
+
+//I know this syntax isn't right. I need to figure that out
+if 
+player.position === 'QB' run QB.JS;
+
+else if
+player.position === 'RB' run  RB.JS;
+
+else if
+player.position === 'WR' run WR.JS;
+
+else if
+player.position === 'TE' run TE.JS;
+
+else return '0'
