64eb0f77e655c9edd2f33deb6df30996bc60fadd
Marnie K <marnie.kelly.123@gmail.com>
2021-04-12 15:10:49 -0400

    setting up switch statement

--- scoring.js
+++ scoring.js
@@ -1,25 +1,36 @@
 function calculateScore(player) {
-//if QB
+
+}
+
+switch(player.position) {
+  case 'QB':
+    //if QB
 //  calculate score for passing stats
 //  calculate score for rushing stats
 //  add together passing and rushing scores and return
-//if RB
+
+  case 'RB':
+    //if RB
 //  calculate score for rushing stats
 //  calculate score for receiving stats
 //  calculate score for kick return stats
 //  calculate score for punt return stats
 //  add together rushing, receiving, kick return, and punt return scores
-//if WR
+
+  case 'WR':
+    //if WR
 //  calculate score for rushing stats
 //  calculate score for receiving stats
 //  calculate score for kick return stats
 //  calculate score for punt return stats
 //  add together rushing, receiving, kick return, and punt return scores
-//if TE
+
+
+  case 'TE':
+    //if TE
 //  calculate score for receiving stats
 //  return receiving score
 // else
-  return 0
-}
-
+  default:
+    return 0}
 module.exports = calculateScore
