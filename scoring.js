61e01548ec0fb2caa7e637413db80ea091e028ac
Marnie K <marnie.kelly.123@gmail.com>
2021-04-12 14:48:17 -0400

    adding pseudocode to scoring.js

---
+++ scoring.js
@@ -0,0 +1,25 @@
+function calculateScore(player) {
+//if QB
+//  calculate score for passing stats
+//  calculate score for rushing stats
+//  add together passing and rushing scores and return
+//if RB
+//  calculate score for rushing stats
+//  calculate score for receiving stats
+//  calculate score for kick return stats
+//  calculate score for punt return stats
+//  add together rushing, receiving, kick return, and punt return scores
+//if WR
+//  calculate score for rushing stats
+//  calculate score for receiving stats
+//  calculate score for kick return stats
+//  calculate score for punt return stats
+//  add together rushing, receiving, kick return, and punt return scores
+//if TE
+//  calculate score for receiving stats
+//  return receiving score
+// else
+  return 0
+}
+
+module.exports = calculateScore
