838d8722dbdba2147fb832bab9a58da78a1e46ed
Marnie K <marnie.kelly.123@gmail.com>
2021-04-12 18:07:21 -0400

    cleared errors

--- scoring.js
+++ scoring.js
@@ -1,40 +1,46 @@
-function calculatePassingScore(){
-  const yards = player.stats.passing.yards/25
+function calculatePassingScore(player){
+  const yards = player.stats.passing.yards / 25
   const touchdowns = player.stats.passing.touchdowns * 6
   const interceptions = player.stats.passing.interceptions * -3
   return yards+touchdowns+interceptions
 }
-function calculateRushingScore(){
-  const yards = player.stats.rushing.yards/10
+function calculateRushingScore(player){
+  const yards = player.stats.rushing.yards / 10
   const touchdowns = player.stats.rushing.touchdowns * 6
   const fumbles = player.stats.rushing.fumbles * -3
   return yards+touchdowns+fumbles
 }
-function calculateReceivingScore(){
-  const yards = player.stats.receiving.yards
-  const 
+function calculateReceivingScore(player){
+  const receptions = player.stats.receiving.receptions * 1
+  const yards = player.stats.receiving.yards / 10
+  const touchdowns = player.stats.receiving.touchdowns * 6
+  const fumbles = player.stats.receiving.fumbles * -3
+  return receptions+yards+touchdowns+fumbles
 }
-function calculateReturnScore(){}
-
+function calculateReturnScore(player){
+const kickYards = player.stats.return.kickreturn.yards / 15
+const kickTouchdowns = player.stats.return.kickreturn.touchdowns * 6
+const kickFumbles = player.stats.return.kickreturn.fumbles * -3
+const puntYards = player.stats.return.puntreturn.yards / 15
+const puntTouchdowns = player.stats.return.puntreturn.touchdowns * 6
+const puntFumbles = player.stats.return.puntreturn.fumbles * -3
+return kickYards+kickTouchdowns+kickFumbles+puntYards+puntTouchdowns+puntFumbles
+}
 function calculateScore(player) {
 switch(player.position) {
 case 'QB':
-return calculatePassingScore()+
-calculateRushingScore()
-
+return calculatePassingScore(player)+
+calculateRushingScore(player)
 case 'RB':
-return calculateRushingScore()+
-calculateReceivingScore()+
-calculateReturnScore()
-
+return calculateRushingScore(player)+
+calculateReceivingScore(player)+
+calculateReturnScore(player)
 case 'WR':
-return calculateRushingScore()+
-calculateReceivingScore()+
-calculateReturnScore()
-
+return calculateRushingScore(player)+
+calculateReceivingScore(player)+
+calculateReturnScore(player)
 case 'TE':
-return calculateReceivingScore()
-
+return calculateReceivingScore(player)
 default:
 return 0}
 }
