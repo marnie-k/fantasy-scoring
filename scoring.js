1bf218a190418f45fa469e523fc45799ff869f9b
Marnie K <marnie.kelly.123@gmail.com>
2021-04-12 16:05:28 -0400

    passing QB test

--- scoring.js
+++ scoring.js
@@ -1,36 +1,41 @@
-function calculateScore(player) {
-
+function calculatePassingScore(){
+  const yards = player.stats.passing.yards/25
+  const touchdowns = player.stats.passing.touchdowns * 6
+  const interceptions = player.stats.passing.interceptions * -3
+  return yards+touchdowns+interceptions
 }
+function calculateRushingScore(){
+  const yards = player.stats.rushing.yards/10
+  const touchdowns = player.stats.rushing.touchdowns * 6
+  const fumbles = player.stats.rushing.fumbles * -3
+  return yards+touchdowns+fumbles
+}
+function calculateReceivingScore(){
+  const yards = player.stats.receiving.yards
+  const 
+}
+function calculateReturnScore(){}
 
+function calculateScore(player) {
 switch(player.position) {
-  case 'QB':
-    //if QB
-//  calculate score for passing stats
-//  calculate score for rushing stats
-//  add together passing and rushing scores and return
+case 'QB':
+return calculatePassingScore()+
+calculateRushingScore()
 
-  case 'RB':
-    //if RB
-//  calculate score for rushing stats
-//  calculate score for receiving stats
-//  calculate score for kick return stats
-//  calculate score for punt return stats
-//  add together rushing, receiving, kick return, and punt return scores
+case 'RB':
+return calculateRushingScore()+
+calculateReceivingScore()+
+calculateReturnScore()
 
-  case 'WR':
-    //if WR
-//  calculate score for rushing stats
-//  calculate score for receiving stats
-//  calculate score for kick return stats
-//  calculate score for punt return stats
-//  add together rushing, receiving, kick return, and punt return scores
+case 'WR':
+return calculateRushingScore()+
+calculateReceivingScore()+
+calculateReturnScore()
 
+case 'TE':
+return calculateReceivingScore()
 
-  case 'TE':
-    //if TE
-//  calculate score for receiving stats
-//  return receiving score
-// else
-  default:
-    return 0}
+default:
+return 0}
+}
 module.exports = calculateScore
