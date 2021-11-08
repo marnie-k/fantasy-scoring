0ccea8f5939c3d21e795ed7a40dbd6721b747631
Marnie K <marnie.kelly.123@gmail.com>
2021-11-07 19:29:25 -0500

    added scoring functions and started switch statement framework

--- index.js
+++ index.js
@@ -1,41 +1,67 @@
 //# Fantasy Scoring
 
-//create array for position
+function passingYds() => {
+  (valueOf(stats.passing.yards)) / 25
+}
+function passingTD() => {
+  (valueOf(stats.passing.touochdowns)) * 6
+}
+function interception() => {
+  (valueOf(stats.passing.interceptions)) * -3
+}
+function rushingYds() => {
+  (valueOf(stats.rushing.yards)) / 10
+}
+function rushingTD() => {
+  (valueOf(stats.rushing.touchdowns)) * 6
+}
+function rushingfumble() => {
+  (valueOf(stats.rushing.fumbles)) * -3
+}
+function receivingYds() => {
+  (valueOf(stats.receiving.yards)) / 10
+}
+function receivingTD() => {
+  (valueOf(stats.receiving.touchdowns)) * 6
+}
+function receivingfumble() => {
+  (valueOf(stats.receiving.fumbles)) * 6
+}
+function kickYds() => {
+  (valueOf(stats.kickreturn.yards)) / 15
+}
+function kickTD() => {
+  (valueOf(stats.kickreturn.touchdowns)) * 6
+}
+function kickFumble() => {
+  (valueOf(stats.kickreturn.fumbles)) * -3
+}
+function puntYds() => {
+  (valueOf(stats.puntreturn.yards)) / 15
+}
+function puntTD() => {
+  (valueOf(stats.puntreturn.touchdowns)) * 6
+}
+function puntFumble() => {
+  (valueOf(stats.puntreturn.fumbles)) * -3
+}
 
-let position = [QB, RB, WR, TE]
+case : player.position =  "QB" {
 
-// top level is an  if...else  
-// if position is in position array go to next level
-// if not (else) return 0
+}
+break;
 
-if position =  "QB"
-  // (passing  yards) / 25
-  // (passing touchdown) * 6
-  // (interception)  * -3
-  // (rushing yards) /10
-  // (rushing touchdown) * 6
-  // (rushing fumble) * -3
-  
-  if position = "RB"  or "WR" 
-  // (rushing yards) /10
-  // (rushing touchdown) * 6
-  // (rushing fumble) * -3
-  // (reception) *  1
-  // (receiving yard) / 10
-  // (receiving TD) *  6
-  // (receiving fumble) * -3
-  // (kick return yards) /15
-  // (kick return TD) * 6
-  // (kick return fumble) * -3
-  // (punt return yards) /15
-  // (punt return TD) *  6
-  // (punt return fumble) * -3
+case : player.position =  "RB" {
 
-  if position = "TE"
- // (reception) *  1
-  // (receiving yard) / 10
-  // (receiving TD) *  6
-  // (receiving fumble) * -3
+}
+break;
+
+case player.position =  "WR" {
+
+}
+case player.position =  "TE" {
+
+}
 
 //  if one  of  these positions add up points e
 // return  sum of points
