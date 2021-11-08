269d00adc9d6075502e0e3eac3489b92e4427976
Marnie K <marnie.kelly.123@gmail.com>
2021-11-08 01:00:12 -0500

    refactored

--- scoring.js
+++ scoring.js
@@ -1,59 +1,50 @@
-const calculateScore = (player) => {
-  switch (player) {
-    case "QB" :
-      return calculateQBScore()
-    case "RB" :
-        return calculateRBScore()
-    case "WR" :
-        return calculateWRScore()
-    case "TE" :
-        return calculateTEScore()
+function calcPassing(player) {
+  const yards = player.stats.passing.yards / 25
+  const tds = player.stats.passing.touchdowns * 6
+  const intercept = player.stats.passing.interceptions * -3
+
+  return yards + tds + intercept
+}
+function calcRushing(player) {
+  const yards = player.stats.rushing.yards / 10
+  const tds = player.stats.rushing.touchdowns * 6
+  const fumbles = player.stats.rushing.fumbles * -3
+
+  return yards + tds + fumbles
+}
+function calcReceiving(player) {
+  const yards = player.stats.receiving.yards / 10
+  const tds = player.stats.receiving.touchdowns * 6
+  const fumbles = player.stats.receiving.fumbles * -3
+  const receptions = player.stats.receiving.receptions * 1
+
+  return yards + tds + fumbles + receptions
+}
+function calcReturns(player) {
+  const kickyards = player.stats.return.kickreturn.yards / 10
+  const kicktds = player.stats.return.kickreturn.touchdowns * 6
+  const kickfumbles = player.stats.return.kickreturn.fumbles * -3
+  const puntyards = player.stats.return.puntreturn.yards / 10
+  const punttds = player.stats.return.puntreturn.touchdowns * 6
+  const puntfumbles = player.stats.return.puntreturn.fumbles * -3
+
+  return kickyards + kickfumbles + kicktds + puntfumbles + punttds + puntyards
+}
+
+
+function calculateScore(player) {
+  switch (player.position) {
+    case 'QB':
+      return calcPassing(player) + calcRushing(player)
+    case 'RB':
+      return calcRushing(player) + calcReceiving(player) + calcReturns(player)
+    case 'WR':
+      return calcRushing(player) + calcReceiving(player) + calcReturns(player)
+    case 'TE':
+      return calcReceiving(player)
     default:
       return 0
-  
-  function calculateQBScore (player) {
-    (player.stats.passing.yards / 25) +
-    (player.stats.passing.touchdowns * 6) +
-    (player.stats.passing.interceptions * -3) +
-    (player.stats.rushing.yards / 10) +
-    (player.stats.rushing.touchdowns * 6) +
-    (player.stats.rushing.fumbles * -3)
   }
-  function calculateRBScore (player) {
-      (player.stats.rushing.yards / 10) +
-      (player.stats.rushing.touchdowns * 6) +
-      (player.stats.rushing.fumbles * -3) +
-      (player.stats.receiving.yards / 10) +
-      (player.stats.receiving.touchdowns * 6) +
-      (player.stats.receiving.fumbles * -3) +
-      player.stats.receiving.receptions +
-      (player.stats.kickreturn.yards / 10) +
-      (player.stats.kickreturn.touchdowns * 6) +
-      (player.stats.kickreturn.fumbles * -3) 
-  }
-  
-  function calculateWRScore (player) {
-          (player.stats.rushing.yards / 10) +
-            (player.stats.rushing.touchdowns * 6) +
-            (player.stats.rushing.fumbles * -3) +
-            (player.stats.receiving.yards / 10) +
-            (player.stats.receiving.touchdowns * 6) +
-            (player.stats.receiving.fumbles * -3) +
-            player.stats.receiving.receptions +
-            (player.stats.kickreturn.yards / 10) +
-            (player.stats.kickreturn.touchdowns * 6) +
-            (player.stats.kickreturn.fumbles * -3) 
-  }
-  function calculateTEScore(player) {
-      (player.stats.receiving.yards / 10) +
-        (player.stats.receiving.touchdowns * 6) +
-        (player.stats.receiving.fumbles * -3) +
-        player.stats.receiving.receptions
-    }
-      }
-    }
-    return calculateScore
+}
 
-    module.exports = calculateScore
-
-  
+module.exports = calculateScore
