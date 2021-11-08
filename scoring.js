4140379bcbe847d1cd87b4006b5d6efde5565beb
Marnie K <marnie.kelly.123@gmail.com>
2021-11-07 23:06:08 -0500

    switch statement is working. need to figure out problem with calculateScore function

--- scoring.js
+++ scoring.js
@@ -1,51 +1,59 @@
 const calculateScore = (player) => {
-  case 
-  (player.position === "QB") {
-    return calculateQBScore(player)
-  }
-  }
-  const calculateQBScore = (player) => {
-    const score = 
-    (player.stats.passing.yards /25) +
+  switch (player) {
+    case "QB" :
+      return calculateQBScore()
+    case "RB" :
+        return calculateRBScore()
+    case "WR" :
+        return calculateWRScore()
+    case "TE" :
+        return calculateTEScore()
+    default:
+      return 0
+  
+  function calculateQBScore (player) {
+    (player.stats.passing.yards / 25) +
     (player.stats.passing.touchdowns * 6) +
     (player.stats.passing.interceptions * -3) +
-    (player.stats.rushing.yards / 10 ) +
+    (player.stats.rushing.yards / 10) +
     (player.stats.rushing.touchdowns * 6) +
-    (player.stats.rushing.fumbles * -3) 
-
-  const calculateRBScore = (player) => {
-    const score = 
-    (player.stats.rushing.yards /10) +
-    (player.stats.rushing.touchdowns * 6) +
-    (player.stats.rushing.fumbles * -3) +
-    (player.stats.receiving.yards / 10 ) +
-    (player.stats.receiving.touchdowns * 6) +
-    (player.stats.receiving.fumbles * -3) +
-    player.stats.receiving.receptions  +
-    (player.stats.kickreturn.yards /10) +
-    (player.stats.kickreturn.touchdowns * 6) +
-    (player.stats.kickreturn.fumbles * -3) +     
-  
-    const calculateWRScore = (player) => {
-      const score = 
-      (player.stats.rushing.yards /10) +
+    (player.stats.rushing.fumbles * -3)
+  }
+  function calculateRBScore (player) {
+      (player.stats.rushing.yards / 10) +
       (player.stats.rushing.touchdowns * 6) +
       (player.stats.rushing.fumbles * -3) +
-      (player.stats.receiving.yards / 10 ) +
+      (player.stats.receiving.yards / 10) +
       (player.stats.receiving.touchdowns * 6) +
       (player.stats.receiving.fumbles * -3) +
-      player.stats.receiving.receptions  +
-      (player.stats.kickreturn.yards /10) +
+      player.stats.receiving.receptions +
+      (player.stats.kickreturn.yards / 10) +
       (player.stats.kickreturn.touchdowns * 6) +
-      (player.stats.kickreturn.fumbles * -3) +     
+      (player.stats.kickreturn.fumbles * -3) 
+  }
   
-      const calculateTEScore = (player) => {
-        const score = 
-        (player.stats.receiving.yards / 10 ) +
+  function calculateWRScore (player) {
+          (player.stats.rushing.yards / 10) +
+            (player.stats.rushing.touchdowns * 6) +
+            (player.stats.rushing.fumbles * -3) +
+            (player.stats.receiving.yards / 10) +
+            (player.stats.receiving.touchdowns * 6) +
+            (player.stats.receiving.fumbles * -3) +
+            player.stats.receiving.receptions +
+            (player.stats.kickreturn.yards / 10) +
+            (player.stats.kickreturn.touchdowns * 6) +
+            (player.stats.kickreturn.fumbles * -3) 
+  }
+  function calculateTEScore(player) {
+      (player.stats.receiving.yards / 10) +
         (player.stats.receiving.touchdowns * 6) +
         (player.stats.receiving.fumbles * -3) +
-        player.stats.receiving.receptions  +
+        player.stats.receiving.receptions
+    }
+      }
+    }
+    return calculateScore
+
+    module.exports = calculateScore
+
   
-  return score
-  }
-  module.exports = calculateScore
